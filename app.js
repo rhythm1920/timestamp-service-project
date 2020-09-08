f31224737971b6719f96c43973111e4bbc37788e
Rhythm <58517101+rhythm1920@users.noreply.github.com>
2020-09-08 22:45:45 +0530

    removed app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let app = express();
-let port = process.env.PORT || 3000 ;
-app.get("/",(req,res)=>{
-    res.send("Test Page");
-});
-app.listen(port,()=>{
-    console.log(`Server started on port ${port}`);
-}) 
